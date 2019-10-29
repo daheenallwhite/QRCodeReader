4b189a1bac8ecd7aa5ed87cb0f12a09ea57e9b9d
Daheen Lee <allwhite.dev@gmail.com>
2019-10-29 21:32:13 +0900

    Add ViewController doing nothing

---
+++ QRCodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCodeReader
+//
+//  Created by Daheen Lee on 13/10/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
